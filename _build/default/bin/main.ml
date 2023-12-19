7a03ba547318d1d45a8be965df6c857a3419bbee
leiDnedyA <aydendiel@gmail.com>
2023-12-19 14:33:54 -0500

    Got basic templating working, added gitignore to get rid of dependencies folder

--- _build/default/bin/main.ml
+++ _build/default/bin/main.ml
@@ -1,30 +1,14 @@
-open Opium
+let name = "Ayden"
 
-let name_page _name =
-  let open Tyxml.Html in
-  html (head (title (txt "Title")) []) (body [])
+let read_file file =
+  In_channel.with_open_bin file In_channel.input_all
 ;;
 
-let index_handler _req =
-  name_page "user"
-  |> name_page
-  |> Response.of_html
-  |> Lwt.return
+let index name =
+  let r = Str.regexp_string "%s" in
+    Str.replace_first r name (read_file "./bin/index.html")
 ;;
 
-let name_handler req =
-  let name = Router.param req "name" in
-  Printf.sprintf "Hello, %s\n" name
-  |> Response.of_plain_text
-  |> Lwt.return
-;;
-
-let () = print_endline "Test"
-
-let _ =
-  App.empty
-  |> App.get "/" index_handler
-  |> App.get "/:name" name_handler
-  |> App.port 8080
-  |> App.run_command
-;;
+let () =
+  Dream.run (fun _ ->
+    Dream.html (index name))
